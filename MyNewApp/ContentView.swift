d8b5e092e30c6f42f8696ef682f538bef3725a5b
Komal Patil <komal.jpatil22@gmail.com>
2021-08-21 13:02:05 +0530

    Initial Commit

---
+++ MyNewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNewApp
+//
+//  Created by Komal Patil on 21/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNewApp/MyNewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewAppApp.swift
+//  MyNewApp
+//
+//  Created by Komal Patil on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MyNewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
